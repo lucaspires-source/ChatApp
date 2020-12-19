3adfabb13445ab8ddeb21e9a49c3c511c0627529
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-19 12:34:27 -0300

    installed bootstrap

---
+++ client/src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container,Form} from 'react-bootstrap'
+const Login = () => {
+    return (
+        <Container>
+            Hello Login
+        </Container>
+    )
+}
+
+export default Login
