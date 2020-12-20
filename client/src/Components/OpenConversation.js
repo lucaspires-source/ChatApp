564b8a60d65a823f9005a33d5f0a04c0285af2f5
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-20 16:05:27 -0300

    almost finished the client side

--- client/src/Components/OpenConversation.js
+++ client/src/Components/OpenConversation.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, {useState,useCallback} from 'react'
 import {Button, Form,InputGroup} from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 const OpenConversation = () => {
     const [text,setText] = useState('')
     const {sendMessage,selectedConversation} = useConversations()
-    
+    const setRef = useCallback(node =>{
+        if (node) {
+            node.scrollIntoView({smooth: true})  
+        }
+    },[])
     const handleSubmit = (e) =>{
         e.preventDefault()
         sendMessage(
@@ -13,11 +17,22 @@ const OpenConversation = () => {
             )
             setText('')
     }
-    
+
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                
+                <div className="d-flex flex-column align-items-start justify-contend-end px-3">
+                    {selectedConversation.messages.map((message, index) => {
+                        const lastMessage = selectedConversation.messages.lenght - 1 === index
+                        return(
+                            <div ref ={lastMessage ? setRef : null}key={index} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}>
+                                <div  className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>{message.text}</div>
+                                <div>{message.fromMe ? 'You' : message.senderName}</div>
+                            </div>
+                        )
+                    })}
+                    
+                </div>
             </div>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="m-2">
